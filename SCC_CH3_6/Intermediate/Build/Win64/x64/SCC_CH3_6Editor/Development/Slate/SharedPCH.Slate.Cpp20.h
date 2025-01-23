a1269bc48cfe68bf749e3370cbb900341973419c
LeemHyungJun <a01085357914@gmail.com>
2025-01-23 11:07:50 +0900

    [ADD] empty project

---
+++ SCC_CH3_6/Intermediate/Build/Win64/x64/SCC_CH3_6Editor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "E:/MY_GAME/UNREAL/SCC_CH3-6_git/SCC_CH3-6/SCC_CH3_6/Intermediate/Build/Win64/x64/SCC_CH3_6Editor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
---
+++ SCC_CH3_6/Source/SCC_CH3_6/SCC_CH3_6.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SCC_CH3_6.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SCC_CH3_6, "SCC_CH3_6" );
